b11a37a458834ec2c4ecb488c1ec20151bd7fa57
lovely singh2819 <loviidovii12@gmail.com>
2023-09-23 21:53:18 +0530

    done

--- models/books_model.js
+++ models/books_model.js
@@ -1,61 +1,15 @@
+const mongo = require("mongoose");
 
-
-
-const mongo=require("mongoose");
-
-
-
-
-
-
-const model_Schema=new mongo.Schema({
-
-
-
-
-    Title:{type:String , require:true},
-
-
-
-    Author:{type:String , require:true},
-
-
-
-    Genre:{type:String , require:true, enum:["Fiction","Science","Comic"]},
-
-
-
-
-    Description:{type:String , require:true},
-
-
-
-
-    Price:{type:Number , require:true}
-
-
-
-
-},{
-
-
-
-
-    versionKey:false
-
-
-
-    
-
+const model_Schema = new mongo.Schema({
+  Title: { type: String, require: true },
+  Author: { type: String, require: true },
+  Genre: { type: String, require: true, enum: ["Fiction", "Science", "Comic"] },
+  Description: { type: String, require: true },
+  Price: { type: Number, require: true }
+}, {
+  versionKey: false
 });
 
+const my_books_model = mongo.model("Book", model_Schema);
 
-
-
-
-const my_books_model=mongo.model("Book",model_Schema); 
-
-
-
-
-module.exports={my_books_model};
+module.exports = { my_books_model };
